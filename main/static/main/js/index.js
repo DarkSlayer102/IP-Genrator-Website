b80c9ac534b9211e5a5cecf1ff1e02c017e49ba2
DarkSlayer <55598746+DarkSlayer102@users.noreply.github.com>
2024-12-25 22:45:42 +0530

    Updated index.js by adding comments to make more understandable

--- main/static/main/js/index.js
+++ main/static/main/js/index.js
@@ -1,23 +1,29 @@
 
-
-
+// Select all list items (assumed to be IPv4 addresses)
 ipv4 = document.querySelectorAll("li");
 
-remove_button = document.querySelector("input")
+// Select the remove button (assumed to be an input element like a button)
+remove_button = document.querySelector("input");
 
+// Initialize a flag to track if the button has been clicked
+let clicked = false;
 
-
-let clicked = false
-remove_button.addEventListener("click", (event)=>{
-    for (let i=0;i< ipv4.length;i++) {
+// Add a click event listener to the remove button
+remove_button.addEventListener("click", (event) => {
+    // Loop through all the list items (IPv4 addresses)
+    for (let i = 0; i < ipv4.length; i++) {
+        // Set the clicked flag to true (indicating the button has been clicked)
         clicked = true;
-        ipv4[i].remove();
-        window.location.href = "/remove"
-        
-    }
-    
 
-})
+        // Remove the current list item (IPv4 address) from the DOM
+        ipv4[i].remove();
+
+        // Redirect the browser to the "/remove" URL
+        // Note: This will happen on the first iteration, so subsequent items won't be removed
+        window.location.href = "/remove";
+    }
+});
+
 
 
 
